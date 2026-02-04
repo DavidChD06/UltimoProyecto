004bd292acb616160c89ff778f63c012c1a3b8c2
Felipe <luisfelipea40@gmail.com>
2026-02-04 14:18:38 +0100

    Avance en TEST

--- test/proyectoCRUD/ui/AccountControllerTest.java
+++ test/proyectoCRUD/ui/AccountControllerTest.java
@@ -5,20 +5,49 @@
  */
 package proyectoCRUD.ui;
 
+import javafx.stage.Stage;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.runners.MethodSorters;
+import static org.testfx.api.FxAssert.verifyThat;
+import org.testfx.framework.junit.ApplicationTest;
+import static org.testfx.matcher.base.NodeMatchers.isEnabled;
+import static org.testfx.matcher.base.NodeMatchers.isVisible;
+import proyectoCRUD.SignInApplication;
 
 /**
  *
  * @author felipe
  */
-public class AccountControllerTest {
-    
-    public AccountControllerTest() {
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class AccountControllerTest extends ApplicationTest {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        new SignInApplication().start(stage);
     }
 
-    @Test
-    public void testSomeMethod() {
+    @Before
+    public void test1_SignInCorrect() {
+        clickOn("#tfEmail");
+        write("jsmith@enterprise.net");
+        clickOn("#pfPassword");
+        write("abcd*1234");
+        verifyThat("#btnLogin", isEnabled());
+        clickOn("#btnLogin");
+        verifyThat("¡Welcome John!", isVisible());
+        clickOn("Aceptar");
     }
-    
+    @Test
+    public void test2_AddAccount() {
+        clickOn("AddAccount");
+        doubleClickOn("#tcDescription");
+        write("New Account");
+        clickOn("#tcBeginBalance");
+        write("3000");
+        clickOn("#Save");
+    }
+
 }
